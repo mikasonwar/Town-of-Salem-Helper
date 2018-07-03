22168b52fa8ec51190636df099d2ddcafbb6b94a
Miguel 'Mikasonwar' Pereira <mikasisto2@gmail.com>
2018-07-03 15:18:18 +0100

    crusader man

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ var Hello = [
  "xin chào"
 ]
 
+var Crusade = [
+  "Deus Vult!",
+  "This is blasphemy.",
+  "By the pope!",
+  "Heresy Town.",
+  "I will cleanse this town of heresy!",
+  "Brethern we need to clean this town!"
+]
 
 $(document).ready(function(){
 
@@ -50,7 +58,7 @@ $(".cover").click(zEsconder);
     role=inno[i][0];
     roleName=inno[i][1].replace(/&rs/g,'<span class="random">').replace(/&re/g, '</span>');
     var lbl = '<div class="'+team+' lblgraveyard" id="'+role+'">'+roleName+'</div>';
-    $(".listagraveyard .flex").append(lbl);
+    $(".listaclaims .flex").append(lbl);
   }
 
 for(var i=0;i<inno.length;i++) {
@@ -60,7 +68,7 @@ for(var i=0;i<inno.length;i++) {
   var lbl = '<label class="'+team+' '+role+'" role="'+role+'"><input type="checkbox" >'+roleName+'</label>';
   $(".roles").append(lbl);
   $(".graveyard").append(lbl);
-  $(".claims").append(lbl);
+  // $(".claims").append(lbl);
 }
 
 for(var i=0;i<maf.length;i++) {
@@ -71,7 +79,7 @@ for(var i=0;i<maf.length;i++) {
   var lbl = '<label class="'+team+' '+role+'" role="'+role+'"><input type="checkbox" >'+roleName+'</label>';
   $(".roles").append(lbl);
   $(".graveyard").append(lbl);
-  $(".claims").append(lbl);
+  // $(".claims").append(lbl);
 }
 
 for(var i=0;i<neut.length;i++) {
@@ -83,7 +91,7 @@ for(var i=0;i<neut.length;i++) {
   var lbl = '<label class="'+team+' '+role+'" role="'+role+'"><input type="checkbox" >'+roleName+'</label>';
   $(".roles").append(lbl);
   $(".graveyard").append(lbl);
-  $(".claims").append(lbl);
+  // $(".claims").append(lbl);
 }
 
   $(".claims").find("label").append("<input class='txtbox' type='text'>")
@@ -104,6 +112,7 @@ for(var i=0;i<neut.length;i++) {
 $(".graveyard").on("change", zAtualizar)
 zAtualizar();
 zHello();
+zCrusade();
 });
 
 function zClick() {
@@ -117,18 +126,45 @@ function zClick() {
   zAtualizar();
 }
 
+
+
 function zAdd() {
-  $(".listagraveyard").show();
-  $(".cover").show();
+  // $(".listagraveyard").show();
+  // $(".listaclaims").show();
+  // $(".cover").show();
+  var texto= "<div class='claimcustom'>"
+           + "<select>";
 
+  for(var i=0;i<inno.length;i++) {
+    roleName=inno[i][1].replace(/&rs/g,'').replace(/&re/g, '');
+    texto+= '<option value="'+inno[i][0]+'">'+roleName+'</option>';
+  }
+  for(var i=0;i<maf.length;i++) {
+    texto+= '<option value="'+maf[i][0]+'">'+maf[i][1]+'</option>';
+  }
+  for(var i=0;i<neut.length;i++) {
+    texto+= '<option value="'+neut[i][0]+'">'+neut[i][1]+'</option>';
+  }
 
+    texto+= "</select>"
+          + "<input type='text'>"
+          + "<a>X</a>"
+          + "</div>";
+
+    $(".claims .flex").append(texto);
+    $(".claims .flex .claimcustom").eq(-1).find("a").click(zRemoverClaim)
 
   // var lbl = '<div class="'+team+' " id="'+role+'">'+roleName+'</div>'
 }
 
 function zEsconder() {
   $(".listagraveyard").hide();
+  $(".listaclaims").hide();
   $(".cover").hide();
+}
+
+function zRemoverClaim() {
+  $(this).parent(".claimcustom").remove();
 }
 
 function zAtualizar() {
@@ -136,6 +172,7 @@ function zAtualizar() {
   var innocent="<span class='inno'>"
   var mafia = "<span class='maf'>"
   var neutral = "<span class='neut'>"
+  var serialkiller = "<span class='sk'>"
   var fim="</span>"
   $(".contagem").empty();
 
@@ -154,6 +191,16 @@ function zAtualizar() {
   if($(".graveyard").find(".townkilling input[type='checkbox']").prop("checked")) {
     mensagemgeral+= innocent+"T"+fim+random+"K"+fim+" está morto\n";
   }
+
+  if(
+    $(".graveyard").find(".godfather input[type='checkbox']").prop("checked") &&
+    $(".graveyard").find(".mafioso input[type='checkbox']").prop("checked") &&
+    $(".graveyard").find(".framer input[type='checkbox']").prop("checked")
+    ) {mensagemgeral+= mafia+"Máfia"+fim+" está morta\n";}
+  if($(".graveyard").find(".serial_killer input[type='checkbox']").prop("checked")){
+    mensagemgeral+= serialkiller+"SK"+fim+" está morto\n";
+  }
+
 
   $(".alertasgerais").append(mensagemgeral);
 }
@@ -176,6 +223,22 @@ function zHello() {
   $(".hello").empty();
   $(".hello").append(texto);
   var clipboard = new ClipboardJS('.hello span');
+  clipboard.on('success', function(e) {
+      console.log(e);
+      zAlerta("Copiado com sucesso");
+  });
+  clipboard.on('error', function(e) {
+      console.log(e);
+  });
+}
+function zCrusade() {
+  var texto="";
+  for(var i=0;i<Crusade.length;i++) {
+    texto += '<span data-clipboard-text="'+Crusade[i]+'"> '+Crusade[i]+'</span>';
+  }
+  $(".crusaderman .flex").empty();
+  $(".crusaderman .flex").append(texto);
+  var clipboard = new ClipboardJS('.crusaderman .flex span');
   clipboard.on('success', function(e) {
       console.log(e);
       zAlerta("Copiado com sucesso");
